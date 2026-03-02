0fa8805158578dd0948cefb97a57a78030be1d9b
Luka Popovic <lukapopovic0111@gmail.com>
2026-03-02 18:06:00 +0100

    Add arithmetic operations example ( +, -, *, / ) in Java

--- src/AritmetickeOperacije.java
+++ src/AritmetickeOperacije.java
@@ -1,2 +1,14 @@
+public class AritmetickeOperacije {
+    public static void main(String[] args) {
+        /// Sabiranje i cake:
+        int a = 10, b = 2;
+        System.out.println("Los rezultata je: " +a+b); //102 ==> zbog texta pre racunanja sve gleda kao string
+        //da bi se ovo gore popravilo stavljamo zagrade ()
+        System.out.println("Pravi rezultat je: " + (a+b));
+        System.out.println("Oduzimanje: "+ (a-b));
+        System.out.println("Mnozenje: "+ (a*b));
+        System.out.println("Deljenje: "+ (a/b));
 
+    }
 
+}
