01f57eb46e76f7df187f521d7531e75ba96a5cab
Luka Popovic <lukapopovic0111@gmail.com>
2026-03-13 21:26:44 +0100

    Add factorial calculation program

--- src/drugaNedelja/Zadtci1.java
+++ src/drugaNedelja/Zadtci1.java
@@ -2,3 +2,19 @@ package drugaNedelja;
 
 import java.util.Scanner;
 
+public class Zadtci1 {
+    //izracunati faktorijal broj(int)
+    //Faktorijal broja je matematička operacija koja znači da pomnožiš taj
+    // broj sa svim pozitivnim celim brojevima manjim od njega, sve do 1.
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Unesite broj");
+        int broj = scanner.nextInt();
+        int rezultata = 1;
+        for(int i = 1; i <= broj; i++ ){
+            rezultata *= i;
+        }
+        System.out.println(rezultata);
+    }
+}
