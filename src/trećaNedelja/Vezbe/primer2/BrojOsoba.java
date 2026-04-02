b57655015dc2d6e9340ee8aa86e27eaa1508b95e
Luka Popovic <lukapopovic0111@gmail.com>
2026-04-02 21:38:43 +0200

    Add BrojOsoba class with static counter for tracking number of persons

--- src/trećaNedelja/Vezbe/primer2/BrojOsoba.java
+++ src/trećaNedelja/Vezbe/primer2/BrojOsoba.java
@@ -1,2 +1,14 @@
 package trećaNedelja.Vezbe.primer2;
 
+public class BrojOsoba {
+    private static int brojOsoba = 0;
+    /// stavljamo STATIC da se vrednosti ne bi menjale od objekta do objekta
+    /// Ovako ova vrednost i ove metode vaze za sve objekte
+    public static void povecajBrojOsoba(){
+        brojOsoba++;
+    }
+
+    public static int getBrojOsoba(){
+        return brojOsoba;
+    }
+}
