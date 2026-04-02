30fc84bad20cc422f67a24abf73e73feffa08c08
Luka Popovic <lukapopovic0111@gmail.com>
2026-04-02 21:40:47 +0200

    Update Osoba class to include Adresa and integrate static person counter

--- src/trećaNedelja/Vezbe/primer2/Osoba.java
+++ src/trećaNedelja/Vezbe/primer2/Osoba.java
@@ -1,2 +1,43 @@
 package trećaNedelja.Vezbe.primer2;
 
+public class Osoba {
+    private String ime;
+    private String prezime;
+    private Adresa adresa;
+
+    public Osoba(String a,String b,Adresa c){
+        ime = a;
+        prezime = b;
+        adresa = c;
+        BrojOsoba.povecajBrojOsoba();
+    }
+
+    public String getIme(){
+        return ime;
+    }
+    public String getPrezime(){
+        return prezime;
+    }
+    public Adresa getAdresa(){
+        return adresa;
+    }
+
+    public void setIme(String newIme){
+        this.ime = newIme;
+    }
+    public void setPrezime(String newPrezime){
+        prezime = newPrezime;
+    }
+    public void setAdresa(Adresa newAdres){
+        adresa = newAdres;
+    }
+
+    @Override
+    public String toString() {
+        return "Osoba{" +
+                "ime='" + ime + '\'' +
+                ", prezime='" + prezime + '\'' +
+                ", adresa=" + adresa + //za adresu poziva Override iz class Adresa
+                '}';
+    }
+}
