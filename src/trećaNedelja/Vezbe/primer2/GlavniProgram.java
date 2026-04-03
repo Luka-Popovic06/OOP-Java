05db639c824f76a9d9596fb6803ac0841c9572fa
Luka Popovic <lukapopovic0111@gmail.com>
2026-04-03 11:38:58 +0200

    Add main program with Scanner input and integration of Osoba, Adresa and BrojOsoba

--- src/trećaNedelja/Vezbe/primer2/GlavniProgram.java
+++ src/trećaNedelja/Vezbe/primer2/GlavniProgram.java
@@ -1,5 +1,37 @@
 package trećaNedelja.Vezbe.primer2;
 
-public class GlavniProgram {
+import java.util.Scanner;
 
+public class GlavniProgram {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String ime;
+        String prezime;
+        String ulica;
+        int broj;
+        System.out.println("Unesite ime:");
+       ime=sc.nextLine();
+        System.out.println("Unesite prezime:");
+       prezime=sc.nextLine();
+        System.out.println("Unesite ulicu:");
+        ulica = sc.nextLine();
+        System.out.println("Unesite br.");
+        broj = sc.nextInt();
+
+        //System.out.println(ime +" "+prezime+" "+ulica+" "+broj);
+
+        Adresa adresa = new Adresa(ulica,broj);
+        System.out.println(adresa);
+        Osoba osoba1 = new Osoba(ime,prezime,adresa);//ovde smo umesto adrese mogli staviti //new Adresa()
+        System.out.println(osoba1);
+
+        ///Ako hocu osobu iz nekog drugog paketa:
+        //moramo da napravimo tacnu rutu
+        trećaNedelja.Vezbe.primer1.Osoba o2=new  trećaNedelja.Vezbe.primer1.Osoba("luka","popovic","123Lu");
+        System.out.println(o2);
+
+        Osoba o3 = new Osoba("jon","bon",new Adresa("Kralja MK.",67));
+
+        System.out.println(BrojOsoba.getBrojOsoba());
+    }
 }
