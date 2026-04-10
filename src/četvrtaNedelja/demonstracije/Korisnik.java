b5562f772fed94b203fbeec11e839bcaf3417760
Luka Popovic <lukapopovic0111@gmail.com>
2026-04-10 18:51:32 +0200

    feat: add Korisnik class

---
+++ src/četvrtaNedelja/demonstracije/Korisnik.java
@@ -0,0 +1,35 @@
+package četvrtaNedelja.demonstracije;
+
+public class Korisnik {
+    private String prikazanoIme;
+    private String korisnickoIme;
+    private String lozinka;
+
+    public Korisnik(String ime,String korisnickoIme,String lozinka){
+        this.prikazanoIme = ime;
+        this.korisnickoIme = korisnickoIme;
+        this.lozinka = lozinka;
+    }
+
+    public void napisiPoruku(Chat chat, String poruka){
+       if(chat.mozeNapisatiPoruku(this)){
+           chat.dodajPoruku(this,poruka);
+       }
+    }
+
+    public String getKorisnickoIme() {
+        return korisnickoIme;
+    }
+
+    public String getPrikazanoIme() {
+        return prikazanoIme;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Korisnik k = (Korisnik) obj;
+
+        return this.korisnickoIme.equals(k.korisnickoIme) && this.prikazanoIme.equals(k.prikazanoIme) && this.lozinka.equals(k.lozinka);
+
+    }
+}
